13db6671a4769a2ca8033149851a91511a38caae
pedroka5 <pedrok5@protonmail.com>
2026-09-21 15:53:18 -0300

    adde some comments

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from fastapi.templating import Jinja2Templates
 import functions as f
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates") ## Vai abrir o diretório 'Templates'
 
 # Pagina Inicial
 @app.get('/', include_in_schema= False)
 def Homepage(request : Request):
-    return templates.TemplateResponse(request,"index.html", {"title":"Home"})
+    return templates.TemplateResponse(request,"index.html", {"title":"Home"}) ### Retorna o valor da variável "title"
 

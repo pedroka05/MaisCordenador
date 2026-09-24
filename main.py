bb49a2d96ea404122871243f9e9c2fb9f0ca862f
pedroka5 <pedrok5@protonmail.com>
2026-09-23 21:08:26 -0300

    add more frontend sites

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ templates = Jinja2Templates(directory="templates") ## Vai abrir o diretório 'Te
 def Homepage(request : Request):
     return templates.TemplateResponse(request,"index.html", {"title":"Home"}) ### Retorna o valor da variável "title"
 
+@app.get('/login_aluno/', include_in_schema= False)
+async def student_login(request: Request):
+    return templates.TemplateResponse(request,"login_aluno.html")
+
+
+
+@app.get('/login_coord/')
+async def coord_login(request: Request):
+    return templates.TemplateResponse(request,"login_coord.html")
+
+
+@app.get('/login_coord/{coord_id}/painel')
+async def coord(request: Request):
+    return templates.TemplateResponse(request, "painel_coord.html")
+
